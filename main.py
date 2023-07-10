11cd7a37d284cab9bc9d1b1ffa9323d5f547404a
David Morais <morais.david@live.com>
2023-07-10 14:26:36 -0300

    better format output

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def verify_update(doc, **kwargs):
     f = open(f"{hash_file}", 'r+')
 
     current_hash = f.read()
-    print(current_hash)
+    # print(current_hash)
 
     if current_hash == hash:
 
@@ -60,8 +60,8 @@ def get_certs_info(**kwargs):
 
     route = f"/api/v1/binarios/{kwargs['cert_type']}/"
     hash_file = f"{kwargs['cert_type']}_hash.txt"
-    print(route)
-    print(hash_file)
+    # print(route)
+    # print(hash_file)
     f = open(f"{hash_file}", "r+")
 
     hash = f.read()
@@ -77,10 +77,10 @@ def get_certs_info(**kwargs):
 
 def convert_base64_to_file(**kwargs):
 
-
-    autoridades_certificadoras_dir = BASE_DIR + '/autoridades_certificadoras/' + f"{kwargs['cert_type']}"
+    autoridades_certificadoras_dir = BASE_DIR + \
+        '/autoridades_certificadoras/' + f"{kwargs['cert_type']}"
     file_base_name = f"{autoridades_certificadoras_dir}/{kwargs['info']['result']['fileName']}"
-    print(kwargs['update_info']['result']['checksum'+f'{kwargs["cert_type"].capitalize()}'])
+    # print(kwargs['update_info']['result']['checksum'+f'{kwargs["cert_type"].capitalize()}'])
 
     with open(f"{file_base_name}", "wb") as _zip:
         _zip.write(base64.b64decode(kwargs['info']['result']['contentBase64']))
@@ -89,7 +89,7 @@ def convert_base64_to_file(**kwargs):
                    '.hash.signed', '.hash.signed.algorithm']
     files = [f"{file_base_name}" + file_sufix for file_sufix in files_sufix]
 
-    print(files)
+    # print(files)
 
     with open(f"{files[0]}", "w+") as _zipHash, open(f"{files[1]}", "w+") as _zipCodec, open(f"{files[2]}", "w+") as _zipSign, open(f"{files[3]}", "w+") as _zipAlg:
         _zipHash.write(kwargs['update_info']['result']
@@ -101,12 +101,18 @@ def convert_base64_to_file(**kwargs):
 
 if __name__ == "__main__":
 
-    print("Update Intermediarias")
+    certs = ['intermediarias', 'confiaveis']
 
     update_info = get_today_info()
 
-    if verify_update(update_info, cert_type='confiaveis'):
-        info = get_certs_info(cert_type='confiaveis')
-        convert_base64_to_file(info=info, update_info=update_info, cert_type='confiaveis')
-    else:
-        print("Intermediarias not updated")
+    for cert_type in certs:
+
+        print(f"Updating {cert_type.capitalize()}.....")
+
+        if verify_update(update_info, cert_type=cert_type):
+            convert_base64_to_file(info=get_certs_info(
+                cert_type=cert_type), update_info=update_info, cert_type=cert_type)
+            with open('last_update.txt', 'w+') as f:
+                f.write(get_date())
+        else:
+            print(f"{cert_type.capitalize()} not updated")
